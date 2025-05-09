1b6926fad1e5a483e713c6a93bfdd36e7c26e15f
Victor Ochoa <victor.silva@luizalabs.com>
2025-05-09 11:30:46 -0300

    add excepition validation

--- src/Linkinho.Domain/Services/LinkService.cs
+++ src/Linkinho.Domain/Services/LinkService.cs
@@ -15,12 +15,18 @@ public class LinkService : ILinkService
 
     public async Task CreateLink(string url)
     {
+        if(string.IsNullOrEmpty(url))
+            throw new ArgumentNullException(nameof(url));
+
         var link = new Link(url);
         await _linkRepository.Create(link);
     }
 
     public async Task<string> GetUrlToRedirect(string identificator)
     {
+        if (string.IsNullOrEmpty(identificator))
+            throw new ArgumentNullException(nameof(identificator));
+
         var link = await _linkRepository.Get(x => x.Identificator == identificator);
 
         if(link is null)
